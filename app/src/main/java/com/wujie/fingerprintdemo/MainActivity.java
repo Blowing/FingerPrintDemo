d652a1fb875a2a7539fa372d1f7592f4e1d96458
Blowing <1072878103@qq.com>
2018-04-27 14:05:39 +0800

    上传界面和权限配置

--- app/src/main/java/com/wujie/fingerprintdemo/MainActivity.java
+++ app/src/main/java/com/wujie/fingerprintdemo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.wujie.fingerprintdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView resultTv;
+    private Button cancelBtn;
+    private Button okBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        resultTv = (TextView) findViewById(R.id.tv_result);
+        okBtn = (Button) findViewById(R.id.btn_ok);
+        cancelBtn = (Button) findViewById(R.id.btn_cancel);
+        okBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_ok:
+                cancelBtn.setEnabled(true);
+                okBtn.setEnabled(false);
+                break;
+            case R.id.btn_cancel:
+                okBtn.setEnabled(true);
+                cancelBtn.setEnabled(false);
+                break;
+            default:
+                break;
+        }
     }
 }
